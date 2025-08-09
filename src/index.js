a251c01c1dc0b955fe0755736dbc7f8da3138fb1
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-08-09 11:30:59 -0300

    Mejorando forms

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router";
-import { UsuarioContext, UsuarioProvider } from "./context/UsuarioContext";
+import { UsuarioProvider } from "./context/UsuarioContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/LoggedScreen.js
+++ src/pages/LoggedScreen.js
@@ -1,14 +1,32 @@
 import React, { useContext } from "react";
 import { UsuarioContext } from "../context/UsuarioContext";
+import Button from "../components/Button/Button";
+import { useNavigate } from "react-router";
 
 export default function LoggedScreen() {
   const { logout, usuario } = useContext(UsuarioContext);
+  const navigate = useNavigate();
+
+  if (!usuario) return null;
+
+  const goToLogin = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <div>
       <h1>LoggedScreen</h1>
       <h2>
         Hola {usuario.nombre} {usuario.apellido}
       </h2>
+
+      <Button
+        onClick={() => {
+          goToLogin();
+        }}
+      >
+        Cerrar Sesión
+      </Button>
     </div>
   );
 }
